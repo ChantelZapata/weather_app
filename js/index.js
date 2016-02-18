e179a3cdfd3827bc6f8fa58453881f4bba1e07e9
ChantelZapata <chantelzapata@gmail.com>
2016-02-18 03:26:43 -0500

    all works

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 
 
-
 function convert() {
  	 var userEntry = $('.js-val').val();
  	 console.log(userEntry);
@@ -9,10 +8,20 @@ function convert() {
  	 
  	 if(result < 10) {
  	 	$('body').css('background-color',"white");
- 	 } else if( result >= 10 && result < 30){
+ 	 } else if(result >= 10 && result < 30){
  	 	$('body').css('background-color',"lightblue");
+ 	 } else if(result >= 30 && result < 60){
+ 	 	$('body').css('background-color',"blue");
+ 	 } else if(result >= 60 && result < 85){
+ 	 	$('body').css('background-color',"green");
+ 	 } else if(result > 85){
+ 	 	$('body').css('background-color',"red");
  	 }
-}
+
+ 	 $('#blankspace').hide();
+ 	 $('#output').show().before(result);
+
+};
 
 $('.js-click').on('click', convert);
 
